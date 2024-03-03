02d7610876e26d0dc4af86c630e02b6e4b1f8695
Nana <frimpongnana8@gmail.com>
2024-03-03 17:43:35 +0000

    created a project page and project component and a projectData json

---
+++ reactprofile/src/components/projectsPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import project from './project'
+import projectData from '../projectData.json'
+
+function projectsPage() {
+  return (
+    <project 
+    
+    
+    
+    
+    
+    
+    
+    
+    />
+  )
+}
+
+export default projectsPage
