988fa46dd7a12b87fc7707e94b204b1ec0d5364c
Nana <frimpongnana8@gmail.com>
2024-03-04 18:00:17 +0000

    cleaned up code

--- reactprofile/src/components/Header.jsx
+++ reactprofile/src/components/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css'
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary header-nav">
+    <nav className="navbar navbar-expand-lg bg-body-dark header-nav">
   <div className="container-fluid">
   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -13,16 +13,16 @@ function Header() {
    
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0 nav-menu" >
-        <li className="nav-item" >
-        <NavLink className="nav-link" activeClassName="active" exact to="/">Home</NavLink>
+        <li className="nav-item " >
+        <NavLink className="nav-link text-white" activeClassName="active" exact to="/">Home</NavLink>
         </li>
         
         <li className="nav-item" >
-        <NavLink className="nav-link" activeClassName="active" to="/portfolio">Portfolio</NavLink>
+        <NavLink className="nav-link text-white" activeClassName="active" to="/portfolio">Portfolio</NavLink>
         </li>
         
         <li className="nav-item" >
-        <NavLink className="nav-link" activeClassName="active" to="/contact">Contact</NavLink>
+        <NavLink className="nav-link text-white" activeClassName="active" to="/contact">Contact</NavLink>
         </li>
     
       </ul>
--- reactprofile/src/components/Projects.jsx
+++ reactprofile/src/components/Projects.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
  import './Project.css'
 
-
-
+ 
 function Projects(props) {
    console.log(props)
   return (
     
        <div className="card"  >
-    <img src={props.image} className="card-img-top" alt="..."/>
+    <img src={props.image} className="card-img-top" alt="..." />
      <div className="card-body">
-      <h5 className="card-title">{props.title}</h5>
-      <a href={props.github} target='_blank' className="btn btn-primary">Github</a>
-      <a href={props.page} target='_blank' className="btn btn-primary">Page</a>
+      <h5 className="card-title text-danger">{props.title}</h5>
+      <a href={props.github} target='_blank' className="btn btn-primary">Github page</a>
+      <a href={props.page} target='_blank' className="btn btn-primary">Deplyod Page</a>
     </div>
   </div>
   
