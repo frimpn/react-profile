6497a03c7a4d8dffbf1c35dfed7efe33ff5f0a14
Nana <frimpongnana8@gmail.com>
2024-03-04 14:56:41 +0000

    added css to header

--- reactprofile/src/components/Header.jsx
+++ reactprofile/src/components/Header.jsx
@@ -1,37 +1,25 @@
 import React from 'react'
 import { NavLink,Link } from 'react-router-dom'
-const header = {
-  navBar: {
-    width: '100vw',
-    display:'flex',
-    justifyContent: 'center',
-    fontSize: 25,
-    },
-    navItem:{
-      marginRight: 40,
-      marginLeft: 40
-    }
-}
+import './Header.css'
+
 
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
    
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+   
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={header.navBar}>
-        <li className="nav-item" style={header.navItem}>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
+        <li className="nav-item" >
         <NavLink className="nav-link" activeClassName="active" exact to="/">Home</NavLink>
         </li>
         
-        <li className="nav-item" style={header.navItem}>
+        <li className="nav-item" >
         <NavLink className="nav-link" activeClassName="active" to="/portfolio">Portfolio</NavLink>
         </li>
         
-        <li className="nav-item" style={header.navItem}>
+        <li className="nav-item" >
         <NavLink className="nav-link" activeClassName="active" to="/contact">Contact</NavLink>
         </li>
     
